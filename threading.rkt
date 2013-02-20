728efa444bbc8ba7add3368818313b88c09290de
Greg Hendershott <greghendershott@gmail.com>
2013-02-19 21:46:23 -0500

    What the previous commit 89d9bc6 would be if it weren't bass-ackwards.

--- threading.rkt
+++ threading.rkt
@@ -31,7 +31,7 @@
     ;; When e is a symbol like 'a, that's actually (quote a), so DON'T
     ;; thread x into that. We want ((quote a) x), NOT (quote x a).
     [(_ x ((~literal quote) e))
-     #'((quote e) x)]
+     #'(x (quote e))]
     [(_ x (e e_1 ...))
      #'(e e_1 ... x)]
     [(_ x form form_1 ...)
