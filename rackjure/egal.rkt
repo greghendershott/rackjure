1bc2a84a2c0371413ffab2bc2632f095d37c6017
Greg Hendershott <greghendershott@gmail.com>
2014-01-24 18:23:11 -0500

    Add unit tests for `egal`

--- rackjure/egal.rkt
+++ rackjure/egal.rkt
@@ -40,3 +40,91 @@
   (or (and (stream-empty? x) (stream-empty? y))
       (and (egal? (stream-first x) (stream-first y))
            (egal-elements? (stream-rest x) (stream-rest y)))))
+
+(module* test racket
+  (require (submod "..")
+           rackunit)
+  (define-syntax (== stx)
+    (syntax-case stx ()
+      [(_ x y) (syntax/loc stx
+                 (check-true (egal? x y)))]))
+  (define-syntax (!= stx)
+    (syntax-case stx ()
+      [(_ x y) (syntax/loc stx
+                 (check-false (egal? x y)))]))
+  ;; boolean?
+  (== #t #t)
+  (!= #t #f)
+  ;; number?
+  (== 0 0)
+  (== 0.0 0.0)
+  (== 1/2 1/2)
+  (!= 0 1)
+  (!= 0.0 1.0)
+  (!= 1/2 2/1)
+  ;; char?
+  (== #\a #\a)
+  (!= #\a #\b)
+  ;; symbol?
+  (== 'a 'a)
+  (!= 'a 'b)
+  ;; box?
+  (let ([a (box 'a)]
+        [b (box 'b)])
+    (== a a)
+    (!= a b))
+  ;; regexp?
+  (== #rx"a" #rx"a")
+  (!= #rx"a" #rx"b")
+  (== #px"a" #px"a")
+  (!= #px"a" #px"b")
+  ;; void?
+  (== (void) (void))
+  ;; Note that `pair`s are _not_ sequence?
+  (== (cons 0 0) (cons 0 0))
+  (!= (cons 0 0) (cons 1 1))
+
+  ;;
+  ;; sequences
+  ;;
+
+  ;; Although "string" literals are immutable, `string` isn't
+  (== "a" "a")
+  (!= "a" "b")
+  (!= (string #\a) (string #\a))
+  (== (string->immutable-string (string #\a))
+      (string->immutable-string (string #\a)))
+
+  ;; Although #"bytes" literals are immutable, `bytes` isn't
+  (== #"a" #"a")
+  (!= #"a" #"b")
+  (!= (bytes 0) (bytes 0))
+  (== (bytes->immutable-bytes (bytes 0))
+      (bytes->immutable-bytes (bytes 0)))
+
+  ;; Although #(0) literals are immutable (as is obviously
+  ;; `vector-immutable`), `vector` isn't.
+  (== #(0) #(0))
+  (!= #(0) #(1))
+  (!= (vector 0) (vector 0))
+  (== (vector-immutable 0) (vector-immutable 0))
+
+  ;; immutable hash variants...
+  (== (hash 0 0) (hash 0 0))
+  (!= (hash 0 0) (hash 0 1))
+  (== (hasheq '0 0) (hasheq '0 0))
+  (!= (hasheq '0 0) (hasheq '0 1))
+  (== (make-immutable-hash '([0 0])) (make-immutable-hash '([0 0])))
+  (!= (make-immutable-hash '([0 0])) (make-immutable-hash '([0 1])))
+  (== (make-immutable-hasheq '([k 0])) (make-immutable-hasheq '([k 0])))
+  (!= (make-immutable-hasheq '([k 0])) (make-immutable-hasheq '([k 1])))
+  ;; mutable hash variants...
+  (!= (make-hash '([0 0])) (make-hash '([0 0])))
+  (!= (make-hasheq '([k 0])) (make-hash '([k 0])))
+
+  ;; stream? is true of many things we test here, but just use `list`
+  (== (list 0 0) (list 0 0))
+  (!= (list 0 0) (list 1 1))
+
+  (== (set 0) (set 0))
+  (!= (set 0) (set 1)))
