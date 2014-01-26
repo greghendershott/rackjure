b28568b555d3e10c9714ec6c0d99c56589e3e816
Greg Hendershott <greghendershott@gmail.com>
2014-01-26 17:25:27 -0500

    Whitespace

--- rackjure/utils.rkt
+++ rackjure/utils.rkt
@@ -7,11 +7,11 @@
 
 (module+ test
   (require rackunit)
-  
+
   ;; If we tested against the variable-arity `+` there would
   ;; be no difference between `partial` and `curry`.
   (define (+* x y) (+ x y))
-  
+
   (check-equal? ((partial +*) 1 2) 3)
   (check-equal? ((partial +* 1) 2) 3)
   (check-equal? ((partial +* 1 2)) 3))
@@ -27,21 +27,21 @@
 (module+ test
   (require rackunit)
   (require racket/future)
-  
+
   (define shared (box 0))
   (define n-iterations 1000)
   (define n-futures 10)
-  
+
   (define (futures)
     (define (thunk)
       (for ([_ (in-range n-iterations)])
         ;; Use `+ 1` instead of `add1` to exercise `box-swap!`
         (box-swap! shared + 1)))
-    
+
     (for/list ([_ n-futures])
       (future thunk)))
-  
+
   (for ([f (futures)])
     (touch f))
-  
+
   (check-equal? (unbox shared) (* n-iterations n-futures)))
