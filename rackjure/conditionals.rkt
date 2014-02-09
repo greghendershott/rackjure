53e69fbba1f108a096698435d1310030ce7fe422
Greg Hendershott <greghendershott@gmail.com>
2014-02-09 18:15:00 -0500

    Reorganize unit tests.
    - Move check-expansion to its own file so that it can be reused. Provide
      both check-expand-once and check-expand-fully variants.
    
    - Split up the big blog of threading tests into smaller test submodules
      interleaved with the functions they test.
    
    - Use check-expand-fully to check the #%app expansion explicitly and
      more concisely.

--- rackjure/conditionals.rkt
+++ rackjure/conditionals.rkt
@@ -19,15 +19,10 @@
   (when (not test) body ...))
 
 (module+ test
-  (require rackunit)
-  ;; Setting `current-namespace` is required for this to work with
-  ;; Racket 5.3.2, although not for later versions like 5.3.5.
-  (define-namespace-anchor anchor)
-  (define (check-expansion input expected-output)
-    (parameterize ([current-namespace (namespace-anchor->namespace anchor)])
-      (check-equal? (syntax->datum (expand-once input))
-                    (syntax->datum expected-output))))
-  (check-expansion #'(if-let [x #t] 0 1)   #'(let [(x #t)] (if x 0 1)))
-  (check-expansion #'(when-let [x #t] 0 1) #'(let [(x #t)] (when x 0 1)))
-  (check-expansion #'(if-not #t 0 1)       #'(if (not #t) 0 1))
-  (check-expansion #'(when-not #t 0 1)     #'(when (not #t) 0 1)))
+  (require rackunit
+           "check-expansion.rkt")
+  (define-namespace-anchor a)
+  (check-expand-once a #'(if-let [x #t] 0 1)   #'(let [(x #t)] (if x 0 1)))
+  (check-expand-once a #'(when-let [x #t] 0 1) #'(let [(x #t)] (when x 0 1)))
+  (check-expand-once a #'(if-not #t 0 1)       #'(if (not #t) 0 1))
+  (check-expand-once a #'(when-not #t 0 1)     #'(when (not #t) 0 1)))
