1fcd90c052fe5090b70c9a8d2a66ccf1833179ca
Greg Hendershott <greghendershott@gmail.com>
2014-06-05 13:42:51 -0400

    Improve comments.

--- rackjure/app.rkt
+++ rackjure/app.rkt
@@ -1,8 +1,10 @@
 #lang racket/base
 
-;; Redefine #%app to:
+;; Provide an alternative `#%app` to:
 ;;
-;; [1] Handle a dict in the first or second position.
+;; [1] Implement applicable `dict?`s.
+;;
+;; (a) Handle a `dict?` in the first or second position.
 ;;
 ;; Downside: Adds a runtime `cond` check to normal applications of
 ;; arities 2 and 3.
@@ -14,23 +16,25 @@
 ;; One issue is how to handle the optional last argument to
 ;; `dict-ref`, which is the value to use if the key is not found. We
 ;; handle this slightly differently than `dict-ref`: 1. We use an
-;; optional _keyword_ argument #:else. This leaves arity 3 available
-;; to mean `dict-set`. 2. If this arg isn't supplied and the key isn't
-;; found, `dict-ref` raises an error. Instead we return `#f`. This is
-;; a more convenient, espeically when used with threading macros -->
-;; and -->>. It's smart that dict-ref lets you supply a specific value
-;; to mean not-found, because what if `#f` or 'not-found or whatever
-;; could be a valid value in the dict?  But even it's smarter to have
-;; some default not-found other than error, and make that be #f.  That
-;; way, burden is only on code that needs to store #f as values in a
-;; dict, to use the #:else keyword.
+;; optional keyword argument #:else. This leaves arity 3 available to
+;; mean `dict-set`. 2. When a default arg isn't supplied and the key
+;; isn't found, `dict-ref` raises an error. Instead we return `#f`.
+;; This is more convenient, especially when used with threading macros
+;; ~> and ~>>. [It's smart that dict-ref lets you supply a specific
+;; value to mean not-found -- because what if `#f` or 'not-found or
+;; whatever could be a valid value in the `dict?`. But even smarter is
+;; for the not-found behavior to be returning #f, by default, rather
+;; than raising an error. That way, using #:else is required only for
+;; the special case of a dict that needs to store #f values.]
 ;;
-;; [1](a) Handle (key #f) as #f. This allows doing a `dict-has-key?`
-;; over nested dicts with threading as (--> dict 'a 'b 'c). Because
+;; (b) Handle `(key #f)` as #f. This allows doing a `dict-has-key?`
+;; over nested dicts with threading as `(~> dict 'a 'b 'c)`. Because
 ;; failure at any point will return #f, and we propogate the #f to the
 ;; end.
 ;;
-;; [2] Expand {k v ... ...} as (dict k v ... ...).
+;; [2] Expand `{k v ... ...}` as `((current-curly-dict) k v ... ...)`.
+;; The current-curly-dict parameter may be e.g. `hash`, `hasheq`,
+;; `alist`.
 
 (provide -#%app
          alist
@@ -46,7 +50,7 @@
 (define (maybe-dict-ref x y)
   (cond [(procedure? x) (#%app    x y)]     ;check normal case first/fast
         [(dict? x)      (dict-ref x y #f)]  ;(dict key)
-        [(not y)        #f]                 ;(key #f) => #F
+        [(not y)        #f]                 ;(key #f) => #f
         [(dict? y)      (dict-ref y x #f)]  ;(key dict)
         [else (error 'applicable-dict
                      "No dict? supplied\nin: (~v ~v)" x y)]))
@@ -64,13 +68,14 @@
         [else (error 'applicable-dict
                      "No dict? supplied\nin: (~a ~a ~a)" x y z)]))
 
-;; What does {} use to initialize? Can be `hash`, `hasheq`, `alist`,
-;; or similar signature function that returns a `dict?`.
+;; What function does `{ k v ... ... }` expand to? Can be `hash`,
+;; `hasheq`, `alist`, or similar signature function that returns a
+;; `dict?`.
 (define current-curly-dict (make-parameter alist))
 
 (define-syntax (-#%app stx)
   (syntax-parse stx
-    ;; { ... } dict literals
+    ;; { key val ... ... } dict literals
     [(_ x:expr ...) #:when (eq? (syntax-property stx 'paren-shape) #\{)
      (unless (zero? (remainder (length (syntax->list #'(x ...))) 2))
        (raise-syntax-error '|{ }|
@@ -82,5 +87,5 @@
     [(_ x:expr y:expr)               #'(maybe-dict-ref x y)]
     [(_ x:expr y:expr #:else d:expr) #'(maybe-dict-ref/else x y #:else d)]
     [(_ x:expr y:expr z:expr)        #'(maybe-dict-set x y z)]
-    ;; The usual
+    ;; Else just the usual Racket #%app
     [(_ f      a ...)                #'(#%app f a ...)]))
