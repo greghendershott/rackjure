985489c08a7213f8bca99afe2effb982baef8f1a
Greg Hendershott <greghendershott@gmail.com>
2014-06-23 13:34:18 -0400

    Use list* not list match pattern.
    Former is faster, for big N.
    
    Also add some unit tests.

--- rackjure/alist.rkt
+++ rackjure/alist.rkt
@@ -6,11 +6,16 @@
 
 (define (alist . xs)
   (match xs
-    [(list) (list)]
-    [(list k v more ...) (cons (cons k v) (apply alist more))]
-    [(list x) (raise-arity-error 'alist (arity-at-least 2) x)]))
+    [(list* k v more) (cons (cons k v) (apply alist more))]
+    [(list x)         (raise-arity-error 'alist (arity-at-least 2) x)]
+    [(list)           (list)]))
 
 (define (alist? xs)
   (and (list? xs)
        (for/and ([x (in-list xs)])
          (pair? x))))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (alist 1 2 3 4 5 6) '([1 . 2][3 . 4][5 . 6]))
+  (check-equal? (alist) '()))
