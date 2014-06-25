3b81fc247d842a3a4bed944aeb405454c6c8ae5d
Greg Hendershott <greghendershott@gmail.com>
2014-06-25 12:30:08 -0400

    Add missing build-deps.
    Consequence of adding Scribble documentation in recent commits.

--- info.rkt
+++ info.rkt
@@ -1,5 +1,9 @@
 #lang setup/infotab
 (define version "0.5")
 (define collection 'multi)
-(define deps '("base" "rackunit-lib"))
-(define build-deps '("rackunit-lib"))
+(define deps '("base"
+               "rackunit-lib"))
+(define build-deps '("rackunit-lib"
+                     "racket-doc"
+                     "sandbox-lib"
+                     "scribble-lib"))
