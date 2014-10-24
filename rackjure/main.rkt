a29259d56728302e46337d381f6ba648c615d5f6
AlexKnauth <alexander@knauth.org>
2014-10-23 21:54:40 -0400

    update current-readtable in #%module-begin so that #fn works in repl

--- rackjure/main.rkt
+++ rackjure/main.rkt
@@ -8,8 +8,9 @@
          "threading.rkt"
          "utils.rkt")
 
-(provide (except-out (all-from-out racket) #%app)
-         (rename-out [-#%app #%app])
+(provide (except-out (all-from-out racket) #%app #%module-begin)
+         (rename-out [-#%app #%app]
+                     [-#%module-begin #%module-begin])
          (except-out (all-from-out "app.rkt") -#%app)
          (all-from-out "alist.rkt"
                        "conditionals.rkt"
@@ -17,3 +18,9 @@
                        "str.rkt"
                        "threading.rkt"
                        "utils.rkt"))
+
+(define-syntax-rule (-#%module-begin form ...)
+  (#%module-begin
+   (require (only-in rackjure/lambda-reader make-lambda-readtable))
+   (current-readtable (make-lambda-readtable (current-readtable)))
+   form ...))
