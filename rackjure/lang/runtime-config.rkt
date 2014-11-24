05e57f77e304d97571cfc0499eec1abaf0b13e85
Greg Hendershott <greghendershott@gmail.com>
2014-11-24 18:20:21 -0500

    Simplify `configure`.
    As discussed here:
    
      https://github.com/greghendershott/rackjure/pull/46#issuecomment-64031796

--- rackjure/lang/runtime-config.rkt
+++ rackjure/lang/runtime-config.rkt
@@ -5,8 +5,4 @@
 (require (only-in rackjure/lambda-reader make-lambda-readtable))
 
 (define (configure data)
-  (define old-read (current-read-interaction))
-  (define (new-read src in)
-    (parameterize ([current-readtable (make-lambda-readtable (current-readtable))])
-      (old-read src in)))
-  (current-read-interaction new-read))
+  (current-readtable (make-lambda-readtable (current-readtable))))
