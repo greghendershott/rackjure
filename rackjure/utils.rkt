2bc5c915f262504c7a47ad8809f63b182276d859
Greg Hendershott <greghendershott@gmail.com>
2015-10-13 21:24:43 -0400

    Simply support `box-swap!`
    Now that we require Racket 6.0 we can stop handling the case where an
    older Racket version doesn't supply `box-cas!`.

--- rackjure/utils.rkt
+++ rackjure/utils.rkt
@@ -1,7 +1,5 @@
 #lang racket/base
 
-(require version/utils)
-
 (provide partial
          box-swap!)
 
@@ -20,7 +18,7 @@
   (check-equal? ((partial +* 1) 2) 3)
   (check-equal? ((partial +* 1 2)) 3))
 
-(define (-box-swap! box f . args)
+(define (box-swap! box f . args)
   (let loop ()
     (let* ([old (unbox box)]
            [new (apply f old args)])
@@ -28,31 +26,21 @@
           new
           (loop)))))
 
-(define (racket-5.92+?)
-  (version<=? "5.92" (version)))
-
-(define box-swap!
-  (cond [(racket-5.92+?) -box-swap!]
-        [(lambda _ (error 'box-swap! "requires Racket 5.92 or newer"))]))
-
 (module+ test
   (require racket/future)
-  (cond [(racket-5.92+?)
-         (define shared (box 0))
-         (define n-iterations 10000000)
-         (define n-futures 10)
+  (define shared (box 0))
+  (define n-iterations 10000000)
+  (define n-futures 10)
 
-         (define (futures)
-           (define (thunk)
-             (for ([_ (in-range n-iterations)])
-               ;; Use `+ 1` instead of `add1` to exercise `box-swap!`
-               (box-swap! shared + 1)))
+  (define (futures)
+    (define (thunk)
+      (for ([_ (in-range n-iterations)])
+        ;; Use `+ 1` instead of `add1` to exercise `box-swap!`
+        (box-swap! shared + 1)))
 
-           (for/list ([_ n-futures])
-             (future thunk)))
+    (for/list ([_ n-futures])
+      (future thunk)))
 
-         (for ([f (futures)])
-           (touch f))
-         (check-equal? (unbox shared) (* n-iterations n-futures))]
-        [else
-         (check-exn exn:fail? (lambda () (box-swap! (box 0) + 1)))]))
+  (for ([f (futures)])
+    (touch f))
+  (check-equal? (unbox shared) (* n-iterations n-futures)))
