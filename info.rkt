a42be6c514e8469cd9fcf348d6a75b862b5bbd3a
Greg Hendershott <git@greghendershott.com>
2020-04-02 14:24:53 -0400

    Require Racket 6.3
    See <https://github.com/racket/racket/pull/3084>.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define version "0.10")
 (define collection 'multi)
-(define deps '(["base" #:version "6.2"]
+(define deps '(["base" #:version "6.3"]
                "rackunit-lib"
                ["threading-lib" #:version "1.1"]))
 (define build-deps '("rackunit-lib"
